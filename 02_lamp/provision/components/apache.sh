8371279c62c6857c0acfc24aa3d90e2849108ec2
kn3sv1 <kn3sv1@example.com>
2022-08-06 16:36:03 +0300

    init

---
+++ 02_lamp/provision/components/apache.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y mc net-tools curl git apache2
+
+
+# Copy the vhost config file
+cp /var/www/provision/config/apache/vhosts/mytestproject.local.com.conf /etc/apache2/sites-available/mytestproject.local.com.conf
+
+# Disable the default vhost file
+a2dissite 000-default
+
+# Enable our custom vhost file
+a2ensite mytestproject.local.com.conf
+
+# Restart for the changes to take effect
+service apache2 restart
---
+++ 02_lamp/provision/components/bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source /var/www/provision/components/apache.sh
+source /var/www/provision/components/mysql.sh
+source /var/www/provision/components/php.sh
+source /var/www/provision/components/phpmyadmin.sh
+source /var/www/provision/components/yarn.sh
